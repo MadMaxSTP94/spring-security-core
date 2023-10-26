d1abe3b7d325273c83686341e3e119ef9abcd1ec
metal <mee>
2023-10-26 23:58:06 +0300

    version 1/0

---
+++ src/main/java/com/makson/springsecuritydemo/SpringSecurityDemostrationApplication.java
@@ -0,0 +1,13 @@
+package com.makson.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemostrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemostrationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/makson/springsecuritydemo/model/Developer.java
@@ -0,0 +1,12 @@
+package com.makson.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstname;
+    private String lastname;
+}
